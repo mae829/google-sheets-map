d035a84bb36f40582d08ea3b9ae1a4d305513b1a
Miguel A. Estrada <mae829@gmail.com>
2021-09-10 16:22:29 -0700

    fix: CSS asset path

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 		<meta charset="UTF-8">
 		<title>Google Sheets Map</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-		<link rel="stylesheet" href="./assets/css/style.min.css">
+		<link rel="stylesheet" href="./assets/css/main.min.css">
 
 		<script>
 			WebFontConfig = {
